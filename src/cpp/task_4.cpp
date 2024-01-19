e5ae8de24830c0ffec13658ef9eb42632a64e39b
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2024-01-19 23:51:41 +0500

    problem4 solved

--- src/cpp/task_4.cpp
+++ src/cpp/task_4.cpp
@@ -5,4 +5,34 @@
  */
 
 class Problem4 {
+    public:
+        ListNode* mergeNodes(ListNode* head) {
+                ListNode* cur= head;
+                ListNode* newHead= NULL;
+                    int sum=0;
+                    while(cur!=NULL){
+                        ewHead->next;
+                                sum=0;if(cur->val !=0){
+                                    sum+=cur->val;
+                                }
+                                else if(sum!=0 && cur->val==0){
+                                    ListNode* node= new ListNode(sum);
+                                    if(newHead==NULL){
+                                        newHead=node;
+                                        head=node;
+                                        sum=0;
+
+                                    }
+                                    else{
+
+                                        newHead->next=node;
+                                        newHead=n
+
+                            }
+                        }
+                        cur=cur->next;
+                    }
+                    return head;
+            }
+
 };
