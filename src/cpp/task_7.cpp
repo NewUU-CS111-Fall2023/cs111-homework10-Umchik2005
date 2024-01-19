c20bfd3f0c0c4f2b587df44b31c5f6cf68862cc5
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2024-01-19 23:53:58 +0500

    problem7 solved

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -5,5 +5,22 @@
  */
 
 class Problem7 {
-public:
+public:int minimumRabbits(vector<int>& answers) {
+        int minRabbits = 0;
+        unordered_map<int, int> answerCounts;
+
+        for (int answer : answers) {
+            answerCounts[answer]++;
+        }
+
+        for (auto& [answer, count] : answerCounts) {
+            if (answer == 0) {
+                minRabbits += 1;
+            } else {
+                minRabbits += answer + (count - 1) / (answer + 1) * (answer + 1);
+            }
+        }
+
+        return minRabbits;
+    }
 };
