c4b3f68812084993c6c49be6e9378527f49d9cf2
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2024-01-19 23:54:31 +0500

    problem8 solved

--- src/cpp/task_8.cpp
+++ src/cpp/task_8.cpp
@@ -6,4 +6,29 @@
 
 class Problem8 {
 public:
+    bool canPartition(vector<int>& nums) {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        // If the total sum is odd, partitioning is not possible.
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+
+        int targetSum = totalSum / 2;
+        vector<bool> dp(targetSum + 1, false);
+        dp[0] = true; // Base case: 0 sum is always achievable
+
+        for (int num : nums) {
+            for (int sum = targetSum; sum >= num; sum--) {
+                if (dp[sum - num]) {
+                    dp[sum] = true; // If a sum can be reached by excluding num, it can also be reached by including num
+                    if (sum == targetSum) {
+                        return true; // Target sum achieved
+                    }
+                }
+            }
+        }
+
+        return false; // No valid partition found
+    }
 };
