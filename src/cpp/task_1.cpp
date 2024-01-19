65e75978f97f1661af2b47bfe4ee84eb2f83d1c8
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2024-01-19 23:48:58 +0500

    Solving 1st problem

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -4,6 +4,47 @@
  * Name:
  */
 
+#include <cstddef>
 class Problem1 {
+    struct ListNode {
+            int val;
+           ListNode *next;
+           ListNode() : val(0), next(nullptr) {}
+           ListNode(int x) : val(x), next(nullptr) {}
+           ListNode(int x, ListNode *next) : val(x), next(next) {}
+         };
 public:
+
+
+
+
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* cur= head;
+        ListNode* newHead= NULL;
+        int sum=0;
+        while(cur!=NULL){
+
+            if(cur->val !=0){
+                sum+=cur->val;
+            }
+            else if(sum!=0 && cur->val==0){
+                ListNode* node= new ListNode(sum);
+                if(newHead==NULL){
+                    newHead=node;
+                    head=node;
+                    sum=0;
+
+                }
+                else{
+
+                    newHead->next=node;
+                    newHead=newHead->next;
+                    sum=0;
+
+                }
+            }
+            cur=cur->next;
+        }
+        return head;
+    }
 };
