d31be08af50a587ce172ef5727fcc19081e81998
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2024-01-19 23:55:47 +0500

    problem10 solved

--- src/cpp/task_10.cpp
+++ src/cpp/task_10.cpp
@@ -6,4 +6,26 @@
 
 class Problem10 {
 public:
+
+    string generate_3sat_formula(int num_variables, int num_clauses) {
+        random_device rd;
+        mt19937 gen(rd());  
+        uniform_int_distribution<> dis(0, num_variables - 1);  
+        uniform_int_distribution<> neg_dis(0, 1); 
+        string formula = "";
+        for (int i = 0; i < num_clauses; ++i) {
+            formula += "(";
+            for (int j = 0; j < 3; ++j) {
+                char variable = 'A' + dis(gen);
+                bool is_negated = neg_dis(gen) == 0;
+                formula += (is_negated) ? "~" : "";
+                formula += variable;
+                formula += (j < 2) ? " | " : "";
+            }
+            formula += ")";
+            formula += (i < num_clauses - 1) ? " & " : "";
+        }
+
+        return formula;
+    }
 };
