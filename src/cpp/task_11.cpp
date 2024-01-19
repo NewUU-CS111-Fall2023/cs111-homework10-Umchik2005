1266756a66d85b62c02e0bf3fd01ce031d7665b3
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2024-01-19 23:56:11 +0500

    problem 11 solved

--- src/cpp/task_11.cpp
+++ src/cpp/task_11.cpp
@@ -6,4 +6,26 @@
 
 class Problem11 {
 public:
+    string generate_3sat_formula(int num_variables, int num_clauses) {
+        random_device rd;
+        mt19937 gen(rd());  // Seed the random number generator
+        uniform_int_distribution<> dis(0, num_variables - 1);  // Distribution for variable selection
+        uniform_int_distribution<> neg_dis(0, 1);  // Distribution for negation
+
+        string formula = "";
+        for (int i = 0; i < num_clauses; ++i) {
+            formula += "(";
+            for (int j = 0; j < 3; ++j) {
+                char variable = 'A' + dis(gen);
+                bool is_negated = neg_dis(gen) == 0;
+                formula += (is_negated) ? "~" : "";
+                formula += variable;
+                formula += (j < 2) ? " | " : "";
+            }
+            formula += ")";
+            formula += (i < num_clauses - 1) ? " & " : "";
+        }
+
+        return formula;
+    }
 };
