b6fb7c3f61251b745bb1f9fed0b30a0671286bd1
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2024-01-19 23:52:55 +0500

    peoblem5 solved

--- src/cpp/task_5.cpp
+++ src/cpp/task_5.cpp
@@ -3,6 +3,23 @@
  * Date:
  * Name:
  */
+#include <vector>
+
 
 class Problem5 {
+public: bool TripletFounder(vector<int>& nums){
+    vector<int> hel;
+    for(int i=0;i<nums.size()-1;i++){
+        hel.push_back(nums[i]+nums[i+1]);
+    }
+    for(int j=0;j<nums.size();j++){
+        for(int i=0;i<hel.size();i++){
+            if(hel[i]+nums[j]==0){
+                return true;
+            }
+        }
+    }
+        return false;
+
+}
 };
