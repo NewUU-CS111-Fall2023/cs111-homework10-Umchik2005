aac8028f4fdcf01237919e288e641e7c0b54ff4a
Muhammad Uamr Toshpo'latov <m.toshpolatov@newuu.uz>
2024-01-19 23:50:34 +0500

    task3 solved

--- src/cpp/task_3.cpp
+++ src/cpp/task_3.cpp
@@ -5,4 +5,26 @@
  */
 
 class Problem3 {
+public: string FindSum(string s) {
+        int head = 0;
+
+        int sum = 0;
+        String answer;
+        while (true) {
+            char symbol =s[head];
+            switch (symbol) {
+                case '1':
+                    if(s[head]!=NULL) {
+                        answer[head]+=char;
+                    }
+                    head++;
+                    break;
+                case '0':
+                    head++;
+                    break;
+
+            }
+        }
+        return answer;
+    }
 };
